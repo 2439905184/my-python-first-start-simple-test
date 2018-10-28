d253fb683adfbed8ef98fed2d596f0dbb9314a8c
2439905184 <suchenglong018@126.com>
2018-10-28 10:08:34 +0800

    fixed encoding on windows now it can display Chinese and English and utf-8 correctly

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-import os
-print("请输入文件路径,必须要加上双引号,暂时只支持文本文件!")
-s=raw_input()
-file=open(s,r)
+#coding=utf-8
+import sys
+type=sys.getfilesystemencoding()
+print("请输入文件路径").decode('utf-8').encode(type)
+file_path=raw_input()
+file=open(file_path)
 #r只读模式
-file.read()
+print(file.read())
 file.close()
