cff73cc3054e023342ffe217a551431817a0325e
2439905184 <suchenglong018@126.com>
2018-11-27 15:58:21 +0800

    1.0 version

--- texteditor.py
+++ texteditor.py
@@ -1,10 +1,32 @@
 #coding=utf-8
-print("欢迎使用python命令行编辑器")
-print("输入r进入只读模式,输入w进入覆盖编辑模式")
-text_in=raw_input()
-if('r' in text_in):
-  print('你选择了r模式')
-  print('请输入要读取的文本文件路径')
-  url=raw_input()
-  file=open(url)
-  print(file.read())
+while(True):
+  print("欢迎使用本编辑器，请输入模式：r为打开只读,a+为追加编辑模式,文件必须存在,输入exit来退出")
+  text_in=raw_input()
+  if('r' in text_in):
+    print("请输入文件路径")
+    url=raw_input()
+    file=open(url)
+    print(file.read())
+    file.close()
+    print("请继续输入进行操作,输入exit来退出")
+    url=raw_input()
+    if('exit' in url):
+      print("欢迎再次使用本系统")
+      break
+    else:
+	  continue
+  if('exit' in text_in):
+	break
+  elif('a+' in text_in):
+    print("请输入文件路径")
+    url=raw_input()
+    file=open(url,'a+')
+    print("输入I正式开始编辑（暂时只支持追加，不支持删除文字）")
+    text_in=raw_input()
+    if('i' in text_in):
+        text_in=raw_input()
+        if(text_in):
+          file.write(text_in)
+          file.close()
+          print("输入完毕，现已自动保存")
+          print("欢迎再次使用本系统")
